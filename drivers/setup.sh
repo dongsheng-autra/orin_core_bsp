353fb6d8c31879790e85684b811cf215ac4aa80b
dongsheng <dongsheng.jin@autra.tech>
2023-07-06 13:50:33 +0800

    modify imx490 to gmsl2_sensors

--- drivers/setup.sh
+++ drivers/setup.sh
@@ -9,10 +9,10 @@ if [ "`sudo lsmod | grep gmsl_max96712`" == "" ]; then
     sudo insmod gmsl_max96712.ko
 fi
 
-if [ "`sudo lsmod | grep imx490`" == "" ]; then
-    sudo insmod imx490.ko
+if [ "`sudo lsmod | grep gmsl2_sensors`" == "" ]; then
+    sudo insmod gmsl2_sensors.ko
 fi
 echo "[module]: insmod done"
 
-echo "[imx490]: v4l2 running"
+echo "[sensors]: v4l2 running"
 v4l2-ctl --set-fmt-video=width=2880,height=1860 --stream-mmap --stream-count=300 -d /dev/video$1
